51c303ac33b08252fa13854c4eee6cabafeb435a
Shingirai Chanakira <sechanakira@yahoo.com>
2018-12-09 17:03:29 +0200

    Added concrete implementation of Person

--- Main.dart
+++ Main.dart
@@ -19,3 +19,16 @@ class Person{
   set lastName(lastName) => _lastName = lastName;
 
 }
+
+class Student implements Person{
+
+  var _firstName;
+  var _lastName;
+
+  get firstName => _firstName;
+  set firstName(firstName) => _firstName = firstName;
+
+  get lastName => _lastName;
+  set lastName(lastName) => _lastName = lastName;
+
+}
